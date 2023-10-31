69bad38a96acc1ee498b653e87400880ff5e5e45
Matt Beruldsen <mberulds@gmail.com>
2023-10-31 18:55:08 +1100

    added show to route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root to: "rocks#index"
   get '/dashboard', to: "pages#dashboard"
 
-  resources :rocks, only: [:create, :edit, :update, :destroy] do
+  resources :rocks, only: [:show, :create, :edit, :update, :destroy] do
     resources :bookings, only: [:new, :create, :show]
   end
 
